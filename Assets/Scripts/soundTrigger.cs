86b9b91fc171e2f21904f4c46325b4b0c8187bfa
Victor Rouquette <victor.rouquette@orange.fr>
2018-01-26 23:31:14 +0100

    initial commir for 3DSoundTests

--- Assets/Scripts/soundTrigger.cs
+++ Assets/Scripts/soundTrigger.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class soundTrigger : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool isLooped;
+    public int loopTime;
+
+    private AudioSource soundSource;
+    private Collider2D[] colliders;
+
+    private void Awake()
+    {
+        soundSource = GetComponent<AudioSource>();
+        colliders = GetComponents<Collider2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
