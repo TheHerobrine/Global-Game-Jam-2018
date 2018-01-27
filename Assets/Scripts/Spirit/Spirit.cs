750a721123ceedc66024e60dd8bf2064823dd775
Paul Niezborala <paulniezbo@gmail.com>
2018-01-27 20:55:57 +0100

    Reimplement lost changes after merge

--- Assets/Scripts/Spirit/Spirit.cs
+++ Assets/Scripts/Spirit/Spirit.cs
@@ -7,9 +7,6 @@ public class Spirit : MonoBehaviour
     public GameObject tracked;
     public float speed;
     public float trackingRadius;
-    public float oscillateFreq = 1f;
-    public float oscillateOffset = 150f;
-    public float oscillateMagnitude = 0.2f;
 
     private Rigidbody2D rb;
 
@@ -25,7 +22,6 @@ public class Spirit : MonoBehaviour
         if (direction.sqrMagnitude > trackingRadius)
         {
             rb.AddForce(new Vector2(direction.x, direction.y) * direction.sqrMagnitude * speed);
-            // transform.position = Vector3.MoveTowards(transform.position, tracked.transform.position, Time.deltaTime * speed);
         }
         else
         {
