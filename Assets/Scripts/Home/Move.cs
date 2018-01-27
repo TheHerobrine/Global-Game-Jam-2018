3fbed5135753d68c5b0ea63cc25ab785ccba5359
Victor Rouquette <victor.rouquette@orange.fr>
2018-01-27 16:17:43 +0100

    phone prefab finished

--- Assets/Scripts/Home/Move.cs
+++ Assets/Scripts/Home/Move.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour {
+
+    public static Move instance = null;
+
     public float acceleration = 1f;
     public Vector3 orientation;
 
@@ -10,12 +13,21 @@ public class Move : MonoBehaviour {
     private Animator animator;
 
     public float moveLimit = 0.1f;
-
+    public bool moveLocked;
     public float minAnimationSpeed = 0.2f;
     public float maxAnimationSpeed = 1.5f;
 
     private void Awake()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
         animator = orientedObject.GetComponent<Animator>();
     }
     // Use this for initialization
@@ -25,6 +37,7 @@ public class Move : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
 
@@ -34,14 +47,17 @@ public class Move : MonoBehaviour {
         //Vector3 Rotation = new Vector3(0f, 0f, Mathf.Acos(deltaX/deltaY));
         if (speed.normalized.sqrMagnitude >= moveLimit)
         {
-            //this.transform.Translate(new Vector3(deltaX, deltaY, 0.0f));
-			this.GetComponent<Rigidbody2D>().velocity = new Vector3(deltaX, deltaY, 0.0f) / Time.deltaTime;
+            if (!moveLocked)
+            {
+                //this.transform.Translate(new Vector3(deltaX, deltaY, 0.0f));
+                this.GetComponent<Rigidbody2D>().velocity = new Vector3(deltaX, deltaY, 0.0f) / Time.deltaTime;
 
-			animator.speed = Mathf.Lerp(minAnimationSpeed, maxAnimationSpeed, speed.normalized.sqrMagnitude);
-            orientation = new Vector3(inputX, inputY, 0f);
-            orientation.Normalize();
-            //rigidBody.MovePosition(rigidBody.position + (Vector2)speed * Time.deltaTime);
-            orientedObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, speed);
+			    animator.speed = Mathf.Lerp(minAnimationSpeed, maxAnimationSpeed, speed.normalized.sqrMagnitude);
+                orientation = new Vector3(inputX, inputY, 0f);
+                orientation.Normalize();
+                //rigidBody.MovePosition(rigidBody.position + (Vector2)speed * Time.deltaTime);
+                orientedObject.transform.rotation = Quaternion.LookRotation(Vector3.forward, speed);
+            }
         }
 		else
 		{
