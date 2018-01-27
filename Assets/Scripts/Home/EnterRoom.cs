4f0c9fdf435f5c9a0ff29508d6829bbff2674b42
Keride <kevin.colombier2@gmail.com>
2018-01-28 00:20:17 +0100

    porte main scene

--- Assets/Scripts/Home/EnterRoom.cs
+++ Assets/Scripts/Home/EnterRoom.cs
@@ -8,15 +8,19 @@ public class EnterRoom : MonoBehaviour
 {
 
     public GameObject Door;
-    public GameObject FogOfwar;
+    public GameObject[] FogOfwar;
 
     private AudioSource DoorSound;
-    private Renderer RendererFogOfwar;
+    private Renderer[] RendererFogOfwar;
     private bool fadeStarted = false;
     private void Awake()
     {
         DoorSound = this.GetComponent<AudioSource>();
-        RendererFogOfwar = FogOfwar.GetComponent<Renderer>();
+        RendererFogOfwar = new Renderer[FogOfwar.Length];
+        for (int i=0; i<FogOfwar.Length; i++)
+        {
+            RendererFogOfwar[i] = FogOfwar[i].GetComponent<Renderer>();
+        }
     }
     // Use this for initialization
     void Start () {
@@ -32,36 +36,46 @@ public class EnterRoom : MonoBehaviour
     {
         if (fadeStarted)
             return;
-        Debug.Log("Trigger");
         if (Input.GetButtonDown("Fire1"))
         {
             fadeStarted = true;
             DoorSound.Play();
-            StartCoroutine("OpenDoor");
-            Color c = RendererFogOfwar.material.color;
-            Debug.Log(c);
-            if(c.a == 1f)
-                StartCoroutine("Fade");
+            StartCoroutine("FadeDoor");
+            for (int i=0; i< RendererFogOfwar.Length; i++)
+            {
+                Color c = RendererFogOfwar[i].material.color;
+                if (c.a == 1f)
+                    StartCoroutine("FadeFogOfWar", i);
+            }
             this.enabled = false;
         }
     }
 
-    IEnumerator OpenDoor()
+    IEnumerator FadeDoor()
     {
-        for (float f = 0f; f < 90; f += 1f)
-        {
-            Door.transform.Rotate(0f, 0f, -1f);
-            yield return null;
-        }
-    }
-
-    IEnumerator Fade()
-    {
-        Color c = RendererFogOfwar.material.color;
+        Renderer DoorRenderer = Door.GetComponent<Renderer>();
+        Color c = DoorRenderer.material.color;
         for (float f = 1f; f >= 0; f -= 0.01f)
         {
             c.a = f;
-            RendererFogOfwar.material.color = c;
+            DoorRenderer.material.color = c;
+            yield return null;
+        }
+        Door.SetActive(false);
+        /*for (float f = 0f; f < 90; f += 1f)
+        {
+            Door.transform.Rotate(0f, 0f, -1f);
+            yield return null;
+        }*/
+    }
+
+    IEnumerator FadeFogOfWar(int i)
+    {
+        Color c = RendererFogOfwar[i].material.color;
+        for (float f = 1f; f >= 0; f -= 0.01f)
+        {
+            c.a = f;
+            RendererFogOfwar[i].material.color = c;
             yield return null;
         }
         this.enabled = false;
