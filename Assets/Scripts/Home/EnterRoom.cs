479f9e2966df3d7abd7bf9178ef05ff1ac993636
Keride <kevin.colombier2@gmail.com>
2018-01-27 03:02:54 +0100

    WIP enterRoom

---
+++ Assets/Scripts/Home/EnterRoom.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnterRoom : MonoBehaviour
+{
+    private Renderer renderer;
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Bidule");
+        Color color = GetComponent<Renderer>().material.color;
+        color.a = 1f;
+
+        GetComponent<Renderer>().material.SetColor("_Color", color);
+    }
+}
