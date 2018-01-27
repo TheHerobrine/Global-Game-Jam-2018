8d88e4fb233c2de6592e83c3a93f615ba3f53de1
Paul Niezborala <paulniezbo@gmail.com>
2018-01-27 23:56:30 +0100

    Fix character animations and sync with footsteps

---
+++ Assets/Scripts/Misc/FootstepHandler.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FootstepHandler : MonoBehaviour
+{
+    public AudioClip[] footstepSounds = new AudioClip[0];
+    public AudioSource source;
+
+    public void PlayFootstepSound(int i)
+    {
+        source.clip = footstepSounds[i];
+        source.Play();
+    }
+}
