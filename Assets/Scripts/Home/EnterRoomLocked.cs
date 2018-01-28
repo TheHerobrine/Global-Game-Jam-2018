3f5f83ba68a355786b450cd2315c53dcb5f237e3
Keride <kevin.colombier2@gmail.com>
2018-01-28 10:51:19 +0100

    WIPfog + door locked

---
+++ Assets/Scripts/Home/EnterRoomLocked.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class EnterRoomLocked : MonoBehaviour
+{
+
+    private AudioSource DoorLocked;
+    
+    private void Awake()
+    {
+        DoorLocked = this.GetComponent<AudioSource>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (Input.GetButton("Fire1"))
+        {
+            DoorLocked.Play();        
+        }
+    }
+}
