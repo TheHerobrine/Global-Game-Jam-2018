087cdfd40ed0665c2a2c653626a0c335a8e0e482
Paul Niezborala <paulniezbo@gmail.com>
2018-01-28 01:27:04 +0100

    Handle roof

---
+++ Assets/Scripts/Home/RoofHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class RoofHandler : MonoBehaviour
+{
+    public EnvironmentManager envManager;
+    public SpriteRenderer roof;
+    public float fadeSpeed = 1f;
+	
+	void Update()
+    {
+        float targetOpacity = envManager.outside ? 1f : 0f;
+        Color c = roof.material.color;
+        c.a += (targetOpacity - c.a) * Time.deltaTime * fadeSpeed;
+        roof.material.color = c;
+    }
+}
