ee4a351e4a66b93d324f74150f786f4e444a1812
Victor Rouquette <victor.rouquette@orange.fr>
2018-01-28 11:16:59 +0100

    scene intro done

---
+++ Assets/Scripts/FadImage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadImage : MonoBehaviour {
+
+    private bool bappear;
+    private bool bdisappear;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(bappear)
+        {
+            GetComponent<CanvasGroup>().alpha += speed * Time.deltaTime;
+            if(GetComponent<CanvasGroup>().alpha >= 1.0f)
+            {
+                bappear = false;
+            }
+        }
+        if (bdisappear)
+        {
+            GetComponent<CanvasGroup>().alpha -= speed * Time.deltaTime;
+            if (GetComponent<CanvasGroup>().alpha <= 0.0f)
+            {
+                bdisappear = false;
+            }
+        }
+    }
+
+    public void appear()
+    {
+        bappear = true;
+    }
+
+    public void disappear()
+    {
+        bdisappear = true;
+    }
+}
---
+++ Assets/Scripts/SceneIntro.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneIntro : MonoBehaviour {
+
+    public GameObject girl1;
+    public float time1;
+    public GameObject girl2;
+    public float time2;
+    public GameObject girl3;
+
+    private bool activate1;
+    private bool activate2;
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if(timer > time1 && !activate1)
+        {
+            activate1 = true;
+            girl1.GetComponent<FadImage>().disappear();
+            girl2.GetComponent<FadImage>().appear();
+        }
+        if (timer > time2 && !activate2)
+        {
+            activate2 = true;
+            girl2.GetComponent<FadImage>().disappear();
+            girl3.GetComponent<FadImage>().appear();
+        }
+    }
+}
